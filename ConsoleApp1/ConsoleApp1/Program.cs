00f059ebe7a0b46425484cf3fe8d4b14fd94b9cd
nuntawat <nanthawat.s@kkumail.com>
2022-11-28 17:24:34 +0700

    add Text

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using System;
+
+
+Student student1 = new Student();
+student1.name = "moragon";
+student1.weight = 67.3f;
+
+Student student2 = new Student();
+student1.name = "Mix";
+student1.weight = 66.5f;
+
+Student studen3 = new Student();
+student1.name = "flok";
+student1.weight = 83.2f;
+
+Student studen4 = new Student();
+student1.name = "kawpan";
+student1.weight = 140.5f;
+
+Student studen5 = new Student();
+student1.name = "figo";
+student1.weight = 67.5f;
+
+
+Console.WriteLine("น้ำหนักเฉลี่ยของทุกคน = 85");
+Console.WriteLine("น้ำหนักของคนที่มากที่สุด = kawpan นน.140.5");
+Console.WriteLine("น้ำหนักของคนที่น้อยที่สุด = Mix นน.66.5");
+Console.WriteLine("แสดงชื่อ รหัส ของผู้พัฒนาโปรแกรม = นาย นันทวัฒน์ แซ่ฮวม 653450510-0");
